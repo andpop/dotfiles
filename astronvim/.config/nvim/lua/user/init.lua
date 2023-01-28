2b9379843e818393b64eefd0172e8abace634c04
Andrey Popov <andpop@mail.ru>
2023-01-29 00:06:13 +0300

    Change astronvim config

--- astronvim/.config/nvim/lua/user/init.lua
+++ astronvim/.config/nvim/lua/user/init.lua
@@ -213,6 +213,9 @@ local config = {
                         -- quick save
                         -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
                 },
+                i = {
+                        ["<F2>"] = { "<esc>:w<cr>", desc = "Save File" },
+                },
                 t = {
                         -- setting a mapping to false will disable it
                         -- ["<esc>"] = false,
